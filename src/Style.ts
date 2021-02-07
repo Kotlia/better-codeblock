ba6165b343a01c0c32a5f73e070e5e9e6c51d8b0
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-02-07 16:52:57 -0500

    Fix enum

---
+++ src/Style.ts
@@ -0,0 +1,9 @@
+export default class Style {
+    static COY = "coy"
+    static DARK = "dark"
+    static FUNKY = "funky"
+    static OKAIDIA = "okaidia"
+    static SOLARIZEDLIGHT = "solarizedlight"
+    static TOMORROW = "tomorrow"
+    static TWILIGHT = "twilight"
+}
