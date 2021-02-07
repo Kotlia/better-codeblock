9e648f6a5bb8c6dc90dd496569ae6331df680085
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-02-07 16:23:11 -0500

    Add write method

--- src/Codeblock.ts
+++ src/Codeblock.ts
@@ -1,11 +1,14 @@
 export default class Codeblock {
 
     language: string
+    code: string | undefined
 
     constructor(language: string) {
         this.language = language
     }
 
-
+    write(code: string) {
+        this.code = code
+    }
 
 }
