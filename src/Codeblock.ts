d82e3503eca30685182e2aeee70793bbe1e397f2
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-02-08 08:13:31 -0500

    Add svg generator

--- src/Codeblock.ts
+++ src/Codeblock.ts
@@ -3,15 +3,17 @@ import syncFetch from 'sync-fetch'
 import fetch from 'node-fetch'
 import fs from "fs";
 import SvgGenerator from "./SvgGenerator";
+import { defaultCSS } from "./DefaultCSS";
 
 export default class Codeblock {
 
     language: string
     content: string | undefined
-    css: string | undefined
+    css: string
 
     constructor(language: string) {
         this.language = language
+        this.css = defaultCSS
     }
 
     code(content: string) {
@@ -36,7 +38,7 @@ export default class Codeblock {
 
     save(writeStream: fs.WriteStream) {
         return new Promise<void>((resolve, reject) => {
-            writeStream.write(SvgGenerator(), () => {
+            writeStream.write(SvgGenerator(this), () => {
                 resolve()
             })
         })
